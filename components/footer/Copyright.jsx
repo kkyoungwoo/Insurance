9d5e5a602d466ca886434bf1e1f30eb91ff4b1b3
coder <bizdb@kakao.com>
2023-08-22 14:45:09 +0900

    8.22

--- components/footer/Copyright.jsx
+++ components/footer/Copyright.jsx
@@ -2,12 +2,17 @@ import {dsnCN} from "../../hooks/helper";
 
 function Copyright({className, ...restProps}) {
     return (
+        <>
         <h5 className={dsnCN(className)} {...restProps}>
             {new Date().getFullYear()} © Made with <span className="love">♥</span>by 
             <a className="link-hover" data-hover-text="워크비자" target="_blank"
                rel="nofollow"
                href="https://workvisa.co.kr">Work Visa</a>
         </h5>
+        <h5>
+            <span style={{marginLeft:"10px"}}>사업자등록번호 : 884-18-01907</span>
+        </h5>
+        </>
     );
 }
 
